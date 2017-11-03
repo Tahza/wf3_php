a88ee869e77450b9e95ab2e6e68da36e2e886934
Alexis <alexis_dussart@hotmail.fr>
2017-11-03 12:42:34 +0100

    Premieres lignes
    first

--- index.php
+++ index.php
@@ -1,13 +1,33 @@
 <!DOCTYPE HTML>
 <html>
 <head>
-  <title>Exemple</title>
+	<title>Exemple</title>
 </head>
 <body>
 
-  <?php
-    echo "Bonjour, premier script PHP !";
-  ?>
+	<?php
+	$nom = 'Dupont';
+	$prenom = 'Jean';
+
+	echo "Bonjour je suis " . $nom . " " . $prenom;
+
+	echo "<br />";
+
+	$age = 38;
+
+	echo "J'ai $age ans";
+
+	echo "<br />";
+
+	if ($age > 18 && $prenom == "Jean") {
+		echo "Majeur";	
+	} elseif($age >= 15){
+		echo "Ado";
+	} else {
+		echo "Enfant";
+	}
+
+	?>
 
 </body>
 </html>
